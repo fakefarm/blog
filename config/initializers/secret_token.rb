735d18bc958c5d2390ed3bf2cb300b3978a014e5
Dave Woodall <dave@hireDave.me>
2013-12-20 16:04:55 -0700

    initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DaveApp::Application.config.secret_key_base = '1e92c09ec869d1794f7603987b06a14b919646cf8ae947ac802030269f645fadeaac7fa97f73d77387f7371e10264366af27aac93e0c8bcd9ef680bc3f63f62e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DaveApp::Application.config.session_store :cookie_store, key: '_daveApp_session'
