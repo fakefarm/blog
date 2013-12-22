732dae4f190fd30a6949861af31f7467b1ad21d9
Dave Woodall <dave@hireDave.me>
2013-12-21 20:15:25 -0700

    Create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,24 @@ class PostsController < ApplicationController
     @title_post = Post.last
     @tags = Post.tags
   end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    end
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :gist, :slug, :hero_image, :thumb_image, :tag, :body)
+  end
 end
